dd5812d7ad90d6842fefeeb4c0882eebceb8ccd0
Clara Hernández <clara.herni@gmail.com>
2020-06-10 20:58:39 -0300

    agrego facha formulario
    Como se va a ver sin tener funciones asociadas todavía

--- src/index.js
+++ src/index.js
@@ -5,6 +5,17 @@ let mes = 07;
 let anio = 2019;
 let base = 'USD';
 
+cargarOpcionesBase();
+
+function cargarOpcionesBase() {
+    fetch('https://api.exchangeratesapi.io/latest')
+        .then((respuesta) => respuesta.json())
+        .then((respuestaJSON) => {
+            Object.keys(respuestaJSON.rates).forEach((moneda) => {
+                $('#base').append(`<option>${moneda}</option>`);
+            });
+        });
+}
 fetch(`https://api.exchangeratesapi.io/${anio}-${mes}-${dia}?base=${base}`)
     .then((respuesta) => respuesta.json())
     .then((respuestaJSON) => {
@@ -12,7 +23,9 @@ fetch(`https://api.exchangeratesapi.io/${anio}-${mes}-${dia}?base=${base}`)
         $('h2').text(`Cambios del día ${respuestaJSON.date} en base ${respuestaJSON.base}`);
 
         Object.keys(respuestaJSON.rates).forEach((moneda) => {
-            $('ul').append(`<li>${moneda}: ${respuestaJSON.rates[moneda]}</li>`);
+            $('#respuesta').append(
+                `<tr><td>${moneda}</td> <td>${respuestaJSON.rates[moneda]}</td></tr>`
+            );
         });
     })
     .catch((error) => console.error('FALLÓ', error));
