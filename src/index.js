7c13e5fe019cd729900a2050b299d4e1e2a77abe
Clara Hernández <clara.herni@gmail.com>
2020-06-10 20:22:09 -0300

    se muestran los cambios en base a valores fijos

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-<reference types="jquery" />;
+/// <reference types="jquery" />
+
+let dia = 30;
+let mes = 07;
+let anio = 2019;
+let base = 'USD';
+
+fetch(`https://api.exchangeratesapi.io/${anio}-${mes}-${dia}?base=${base}`)
+    .then((respuesta) => respuesta.json())
+    .then((respuestaJSON) => {
+        console.log(respuestaJSON);
+        $('h2').text(`Cambios del día ${respuestaJSON.date} en base ${respuestaJSON.base}`);
+
+        Object.keys(respuestaJSON.rates).forEach((moneda) => {
+            $('ul').append(`<li>${moneda}: ${respuestaJSON.rates[moneda]}</li>`);
+        });
+    })
+    .catch((error) => console.error('FALLÓ', error));
