aa63ed0188b034df0c942a7db29e79971b2bf3c2
Clara Hernández <clara.herni@gmail.com>
2020-06-11 12:47:17 -0300

    testea los cambios y  que se muestre bien

--- cypress/integration/casa-cambio-test.js
+++ cypress/integration/casa-cambio-test.js
@@ -22,11 +22,30 @@ context('Casa de Cambio', () => {
         });
 
         it('verifica el funcionamiento para validar la fecha inexistente por año bisiesto', () => {
-            cy.get('#dia').type('{backspace}{backspace}29');
-            cy.get('#anio').type('{backspace}{backspace}19');
+            cy.get('#dia').clear().type('29');
+            cy.get('#anio').clear().type('2019');
             cy.get('#boton-mostrar').click();
 
             cy.get('#error').should('have.text', 'Esa fecha no existe.');
+        });
+        it('verifica que estén los cambios USD, AUD y CAD', () => {
+            cy.get('select').contains('USD');
+            cy.get('select').contains('AUD');
+            cy.get('select').contains('CAD');
+        });
+    });
+    describe('muestra los cambios', () => {
+        it('se asegura que se muestra la tabla', () => {
+            cy.get('#dia').type('{backspace}{backspace}28');
+            cy.get('#boton-mostrar').click();
+
+            cy.expect('#pantalla-cambio').to.not.have.class('oculto');
+        });
+
+        it('se asegura que se muestren EUR, RUB y BRL', () => {
+            cy.get('thead').contains('EUR');
+            cy.get('thead').contains('RUB');
+            cy.get('thead').contains('BRL');
         });
     });
 });
