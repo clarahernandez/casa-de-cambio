21e04cfe3e250d65df34ac1d7b2aa945e95ddfd7
Clara Hernández <clara.herni@gmail.com>
2020-06-17 14:56:24 -0300

    Arreglo test en base al nuevo input

--- cypress/integration/casa-cambio-test.js
+++ cypress/integration/casa-cambio-test.js
@@ -8,35 +8,14 @@ context('Casa de Cambio', () => {
     });
 
     describe('valida el formulario', () => {
-        it('se asegura que haya un formulario', () => {
-            cy.get('#pantalla-formulario').find('.form-group').should('have.length', 4);
+        it('se asegura que haya un formulario con 2 inputs', () => {
+            cy.get('#pantalla-formulario').find('.form-group').should('have.length', 2);
         });
 
-        it('verifica el funcionamiento para validar la fecha inexistente', () => {
-            cy.get('#dia').type('30');
-            cy.get('#mes').type('2');
-            cy.get('#anio').type('2020');
-            cy.get('#boton-mostrar').click();
-
-            cy.get('#error').should('have.text', 'Esa fecha no existe.');
-        });
-
-        it('verifica el funcionamiento para validar la fecha inexistente por año bisiesto', () => {
-            cy.get('#dia').clear().type('29');
-            cy.get('#anio').clear().type('2019');
-            cy.get('#boton-mostrar').click();
-
-            cy.get('#error').should('have.text', 'Esa fecha no existe.');
-        });
-        it('verifica que estén los cambios USD, AUD y CAD', () => {
-            cy.get('select').contains('USD');
-            cy.get('select').contains('AUD');
-            cy.get('select').contains('CAD');
-        });
     });
     describe('muestra los cambios', () => {
         it('se asegura que se muestra la tabla', () => {
-            cy.get('#dia').type('{backspace}{backspace}28');
+            cy.get('#dia').type('20-05-2020');
             cy.get('#boton-mostrar').click();
 
             cy.expect('#pantalla-cambio').to.not.have.class('oculto');
