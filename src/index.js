c85780bc32651bc86ac5ba61df3832fcf9d75672
Clara Hernández <clara.herni@gmail.com>
2020-06-17 14:43:50 -0300

    Cambio el input de la fecha y agrego eslint

--- src/index.js
+++ src/index.js
@@ -1,96 +1,73 @@
-/// <reference types="jquery" />
 
-cargarOpcionesBase();
-$('#boton-mostrar').click(function () {
-    const anio = $('#anio').val();
-    const mes = $('#mes').val();
-    const dia = $('#dia').val();
-    const base = $('#base').val();
 
-    if (validarFecha(Number(dia), Number(mes), Number(anio))) {
-        limpiarPantallaCambios();
-        $('#error').text('');
-        mostrarPantallaCambios();
-        mostrarCambios(anio, mes, dia, base);
-    } else {
-        $('#error').text('Esa fecha no existe.');
-    }
+document.querySelector('#boton-mostrar').onclick = (() => {
+  const $fecha = document.querySelector('#fecha').value;
+  const $base = document.querySelector('#base').value;
+
+  limpiarPantallaCambios();
+  mostrarPantallaCambios();
+  mostrarCambios($fecha, $base);
 });
 
 function cargarOpcionesBase() {
-    fetch('https://api.exchangeratesapi.io/latest')
-        .then((respuesta) => respuesta.json())
-        .then((respuestaJSON) => {
-            Object.keys(respuestaJSON.rates).forEach((moneda) => {
-                $('#base').append(`<option>${moneda}</option>`);
-            });
-        })
-        .catch((error) => console.error('FALLÓ', error));
+  fetch('https://api.exchangeratesapi.io/latest')
+    .then((respuesta) => respuesta.json())
+    .then((respuestaJSON) => {
+      Object.keys(respuestaJSON.rates).forEach((moneda) => {
+        const $cambio = document.createElement('option');
+        $cambio.textContent = moneda;
+        document.querySelector('#base').appendChild($cambio);
+      });
+    })
+    .catch((error) => console.error('FALLÓ', error));
 }
 
-function mostrarCambios(anio, mes, dia, base) {
-    fetch(`https://api.exchangeratesapi.io/${anio}-${mes}-${dia}?base=${base}`)
-        .then((respuesta) => respuesta.json())
-        .then((respuestaJSON) => {
-            $('h2').text(`Cambios del día ${respuestaJSON.date} en base ${respuestaJSON.base}`);
+function mostrarCambios(fecha, base) {
+  fetch(`https://api.exchangeratesapi.io/${fecha}?base=${base}`)
+    .then((respuesta) => respuesta.json())
+    .then((respuestaJSON) => {
+      document.querySelector('h2').textContent = `Cambios del día ${respuestaJSON.date} en base ${respuestaJSON.base}`;
 
-            Object.keys(respuestaJSON.rates).forEach((moneda) => {
-                $('thead').append(
-                    `<tr><td>${moneda}</td> <td>${respuestaJSON.rates[moneda]}</td></tr>`
-                );
-            });
-        })
-        .catch((error) => console.error('FALLÓ', error));
+      Object.keys(respuestaJSON.rates).forEach((moneda) => {
+        const $fila = document.createElement('tr');
+        const $moneda = document.createElement('td');
+        const $cambio = document.createElement('td');
+        $moneda.textContent = moneda;
+        $cambio.textContent = respuestaJSON.rates[moneda];
+        $fila.appendChild($moneda);
+        $fila.appendChild($cambio);
+        document.querySelector('thead').appendChild($fila);
+      });
+    });
+}
+
+function limitarFechas() {
+  const $fecha = document.querySelector('#fecha');
+  let hoy = new Date();
+  let dd = hoy.getDate();
+  let mm = hoy.getMonth() + 1; // Enero empieza en 0
+  const yyyy = hoy.getFullYear();
+  if (dd < 10) {
+    dd = `0${dd}`;
+  }
+  if (mm < 10) {
+    mm = `0${mm}`;
+  }
+  hoy = `${yyyy}-${mm}-${dd}`;
+  $fecha.setAttribute('max', hoy);
 }
 
 function mostrarPantallaCambios() {
-    $('#pantalla-cambio').removeClass('oculto');
-}
-
-function ocultarPantallaFormulario() {
-    $('#pantalla-formulario').addClass('oculto');
+  document.querySelector('#pantalla-cambio').classList.remove('oculto');
 }
 
 function limpiarPantallaCambios() {
-    $('h2').text('');
-    $('thead').text('');
+  document.querySelector('h2').textContent = '';
+  document.querySelector('thead').textContent = '';
 }
 
-function validarFecha(dia, mes, anio) {
-    const fechas = {
-        1: 31,
-        3: 31,
-        4: 30,
-        5: 31,
-        6: 30,
-        7: 31,
-        8: 31,
-        9: 30,
-        10: 31,
-        11: 30,
-        12: 31,
-    };
-    if (esBisiesto(anio)) {
-        fechas[2] = 29;
-    } else {
-        fechas[2] = 28;
-    }
-
-    if (fechas[mes] >= dia) {
-        return true;
-    } else {
-        return false;
-    }
+function init() {
+  limitarFechas();
+  cargarOpcionesBase();
 }
-
-function esBisiesto(anio) {
-    if (anio % 4 === 0) {
-        if (anio % 100 === 0 && anio % 400 !== 0) {
-            return false;
-        } else {
-            return true;
-        }
-    } else {
-        return false;
-    }
-}
+init();
